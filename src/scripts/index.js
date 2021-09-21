3f88607d83f44832028f4cb28bec4b3159cd4745
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-20 20:19:38 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,24 @@
-import { fromEvent } from "rxjs";
+import { fromEvent } from 'rxjs';
+import { map, tap, filter } from 'rxjs/operators';
+
+// elems
+const codeElem = document.getElementById('code');
+
+// streams
+const keyup$ = fromEvent(document, 'keyup');
+const keycode$ = keyup$.pipe(
+  map(event => event.code),
+  // tap(code => {
+  //   codeElem.innerHTML = code;
+  // })
+);
+
+const enter$ = keycode$.pipe(
+  filter(code => code === 'Enter')
+)
+
+enter$.subscribe(console.log);
+
+// keycode$.subscribe(code => {
+//   codeElem.innerHTML = code;
+// })
