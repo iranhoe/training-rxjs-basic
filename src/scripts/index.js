fb397163aa27e0194b9709baedeccb88df64ede8
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-20 20:12:02 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,16 @@
-import { fromEvent } from "rxjs";
+import { of } from "rxjs";
+import { map, tap } from 'rxjs/operators';
+
+const numbers$ = of(1,2,3,4,5);
+
+numbers$.pipe(
+  tap(value => console.log('before', value)),
+  map(value => value * 10),
+  tap({
+    next: value => console.log('after', value),
+    complete: () => console.log('done!'),
+    error: error => {/*something here*/}
+  }),
+).subscribe(value => {
+  console.log('from subscribe', value)
+});
