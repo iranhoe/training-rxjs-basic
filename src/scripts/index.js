702ccbcff53ff7b72d8ca26a91268e00cbac2431
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-20 20:05:22 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,26 @@
-import { fromEvent } from "rxjs";
+import { interval } from "rxjs";
+import { scan, mapTo, filter } from 'rxjs/operators';
+
+// elem refs
+const countdown = document.getElementById(
+  'countdown'
+);
+const message = document.getElementById(
+  'message'
+);
+
+// streams
+const counter$ = interval(1000);
+
+counter$.pipe(
+  mapTo(-1),
+  scan((accumulator, current) => {
+    return accumulator + current;
+  }, 10),
+  filter(value => value >= 0)
+).subscribe(value => {
+  countdown.innerHTML = value;
+  if (!value) {
+    message.innerHTML = 'Liftoff!';
+  }
+});
