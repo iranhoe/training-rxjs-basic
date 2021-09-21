2ae9dcd78586dbc9c9c1e2c0f9339130716338cd
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-20 19:57:05 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,21 @@
-import { fromEvent } from "rxjs";
+import { from } from "rxjs";
+import { reduce, scan, map } from 'rxjs/operators';
+
+const numbers = [1,2,3,4,5];
+const user = [
+  { name: 'Brian', loggedIn: false, token: null },
+  { name: 'Brian', loggedIn: false, token: 'abc' },
+  { name: 'Brian', loggedIn: false, token: '123' }
+]
+
+const state$ = from(user).pipe(
+  scan((accumulator, currentValue) => {
+    return { ...accumulator, ...currentValue };
+  }, {})
+);
+
+const name$ = state$.pipe(
+  map(state => state.name)
+);
+
+name$.subscribe(console.log);
