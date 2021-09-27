73916448558d49251159d855510a1dcf8c2f9c53
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-27 08:18:26 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-import { fromEvent } from "rxjs";
+import { interval, fromEvent } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
+const counter$ = interval(1000);
+const click$ = fromEvent(document, 'click');
+
+counter$.pipe(
+    takeUntil(click$)
+).subscribe(console.log);
