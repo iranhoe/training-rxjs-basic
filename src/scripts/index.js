37ddc4760f2c5eb4d0fdd370e9f1604870192cb9
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-27 09:37:35 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-import { fromEvent } from "rxjs";
+import { of } from "rxjs";
+import { distinctUntilChanged } from "rxjs/operators";
+
+const numbers$ = of(1, 1, 2, 3, 3, 3, 4, 5, 3);
+
+numbers$.pipe(
+    distinctUntilChanged()
+)
+.subscribe(console.log);
