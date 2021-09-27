45271ab063ec5aaceb08fc9e52fc5fc8c4052afb
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-27 11:44:04 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
 import { fromEvent } from "rxjs";
+import { debounceTime, throttleTime } from 'rxjs/operators';
+
+// streams
+const click$ = fromEvent(document, 'click');
+
+click$.pipe(
+    throttleTime(3000)
+).subscribe(console.log)
