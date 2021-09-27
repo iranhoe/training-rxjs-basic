f07b7d1e1291dd0d08557fb039e53463365682b7
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-27 11:40:19 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,20 @@
-import { fromEvent } from "rxjs";
+import { interval, fromEvent } from "rxjs";
+import { debounceTime, 
+    pluck, 
+    distinctUntilChanged, 
+    debounce } from "rxjs/operators";
+
+// elems
+const inputBox = document.getElementById(
+    'text-input'
+);
+
+// streams
+const click$ = fromEvent(document, 'click');
+const input$ = fromEvent(inputBox, 'keyup');
+
+input$.pipe(
+    debounce(() => interval(1000)),
+    pluck('target', 'value'),
+    distinctUntilChanged()
+).subscribe(console.log);
