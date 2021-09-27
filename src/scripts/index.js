49f7bd94545345743cbc2b36ce4a51fda36fc00a
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-27 08:08:14 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,17 @@
-import { fromEvent } from "rxjs";
+import { of, fromEvent } from "rxjs";
+import { take, map, first } from "rxjs/operators";
+
+const numbers$ = of(1,2,3,4,5);
+const click$ = fromEvent(document, 'click');
+
+click$.pipe(
+    map(event => ({
+        x: event.clientX,
+        y: event.clientY
+    })),
+    // filter, take(1)
+    first(({y}) => y > 200)
+).subscribe({
+    next: console.log,
+    complete: () => console.log('complete!')
+});
