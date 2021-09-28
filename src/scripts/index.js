a19edf71936fc35aea7239c4953d398d3d4493aa
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-27 20:07:55 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
 import { fromEvent } from "rxjs";
+import { auditTime } from 'rxjs/operators';
+
+const click$ = fromEvent(document, 'click');
+
+click$.pipe(
+    auditTime(4000)
+).subscribe(console.log)
