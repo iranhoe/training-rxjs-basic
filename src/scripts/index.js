bd9ee475bc2b94513efe7a7762fda8f0228c5e11
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-28 17:26:54 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-import { fromEvent } from "rxjs";
+import { fromEvent, interval } from "rxjs";
+import { switchMap } from 'rxjs/operators';
+
+const interval$ = interval(1000);
+const click$ = fromEvent(document, 'click');
+
+click$.pipe(
+    switchMap(() => interval$)
+).subscribe(console.log);
