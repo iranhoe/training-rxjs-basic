30c26b4a84f8c4428fd397ca2cafc4ae61d538f5
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-09-28 17:15:24 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
 import { fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { map, debounceTime, mergeAll, mergeMap } from 'rxjs/operators';
+
+// elems
+const textInput = document.getElementById('text-input');
+
+// streams
+const input$ = fromEvent(textInput, 'keyup');
+
+input$.pipe(
+    debounceTime(200),
+    mergeMap(event => {
+        const term = event.target.value
+        return ajax.getJSON(
+            `https://api.github.com/users/${term}` 
+        )
+    })
+).subscribe(console.log);
