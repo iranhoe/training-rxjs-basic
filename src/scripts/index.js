30df3f0e4b05777e69891dd691e79ca078b655b2
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-08 17:51:51 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-import { fromEvent } from "rxjs";
+import { fromEvent, interval } from "rxjs";
+import { exhaustMap,  take } from 'rxjs/operators';
+
+const interval$ = interval(1000);
+const clicks$ = fromEvent(document, 'click');
+
+clicks$.pipe(
+    exhaustMap(() => interval$.pipe(take(3)))
+).subscribe(console.log);
