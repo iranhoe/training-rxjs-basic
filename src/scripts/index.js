b171ebde3a384729c8b9cef70c5407fe0e83e5d1
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-08 18:40:50 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-import { fromEvent } from "rxjs";
+import { of } from "rxjs";
+import { startWith, endWith } from "rxjs/operators";
+
+const numbers$ = of(1,2,3);
+
+numbers$.pipe(
+    startWith('a', 'b', 'c'),
+    endWith('a', 'b', 'c')
+).subscribe(console.log);
