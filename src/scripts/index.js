3f03db0bc9634c9d8a4940659fb079bb56a1a1c6
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-12 16:44:31 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,27 @@
-import { fromEvent } from "rxjs";
+import { fromEvent, combineLatest } from "rxjs";
+import { map, filter } from 'rxjs/operators';
+
+// elems
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+
+// streams
+const keyup$ = fromEvent(document, 'keyup');
+const click$ = fromEvent(document, 'click');
+
+// helpers
+const keyupAsValue = elem => {
+    return fromEvent(elem, 'keyup').pipe(
+        map(event => event.target.valueAsNumber)
+    )
+};
+
+combineLatest(
+    keyupAsValue(first),
+    keyupAsValue(second)
+).pipe(
+    filter(([first,second]) => {
+        return !isNaN(first) && !isNaN(second);
+    }),
+    map(([first, second]) => first + second)
+).subscribe(console.log);
