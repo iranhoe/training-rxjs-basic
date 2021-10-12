d915317f22f53804ec8b582eb8d5b9e5e9bcbcb5
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-11 18:15:36 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,12 @@
-import { fromEvent } from "rxjs";
+import { fromEvent, merge } from "rxjs";
+
+const keyup$ = fromEvent(document, 'keyup');
+const click$ = fromEvent(document, 'click');
+
+// keyup$.subscribe(console.log);
+// click$.subscribe(console.log);
+
+merge(
+    keyup$,
+    click$
+).subscribe(console.log);
