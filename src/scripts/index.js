147499c94980d53588eda205c2b7b159827bfff3
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-12 16:14:04 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,14 @@
-import { fromEvent, interval, merge, EMPTY } from "rxjs";
-import { takeWhile, startWith, scan, mapTo, switchMap } from "rxjs/operators";
+import { interval, EMPTY } from 'rxjs';
+import { concat, startWith, delay } from 'rxjs/operators';
 
-const COUNTDOWN_FROM = 10;
+const delayed$ = EMPTY.pipe(delay(1000));
 
-const countdown = document.getElementById('countdown');
-const message = document.getElementById('message');
-const pauseButton = document.getElementById('pause');
-const startButton = document.getElementById('start');
-
-const counter$ = interval(1000);
-const startClick$ = fromEvent(startButton, 'click');
-const pauseClick$ = fromEvent(pauseButton, 'click');
-
-merge(
-    startClick$.pipe(mapTo(true)),
-    pauseClick$.pipe(mapTo(false))
-).pipe(
-    switchMap(shouldStart => {
-        return shouldStart ? counter$ : EMPTY
-    }),
-    mapTo(-1),
-    scan((accumulator, current) => {
-        return accumulator + current;
-    }, COUNTDOWN_FROM),
-    takeWhile(value => value >= 0),
-    startWith(COUNTDOWN_FROM)
-).subscribe(value => {
-    countdown.innerHTML = value;
-    if (!value) {
-        message.innerHTML = 'Liftoff!!';
-    }
-})
+delayed$.pipe(
+    concat(
+        delayed$.pipe(startWith('3...')),
+        delayed$.pipe(startWith('2...')),
+        delayed$.pipe(startWith('1...')),
+        delayed$.pipe(startWith('Go!'))
+    ),
+    startWith('Get Ready!')
+).subscribe(console.log);
