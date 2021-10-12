f449aad8333d66c72b09cee1f51f8790edc9e62d
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-11 18:31:32 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,33 @@
-import { fromEvent, merge } from "rxjs";
+import { fromEvent, interval, merge, EMPTY } from "rxjs";
+import { takeWhile, startWith, scan, mapTo, switchMap } from "rxjs/operators";
 
-const keyup$ = fromEvent(document, 'keyup');
-const click$ = fromEvent(document, 'click');
+const COUNTDOWN_FROM = 10;
 
-// keyup$.subscribe(console.log);
-// click$.subscribe(console.log);
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+const pauseButton = document.getElementById('pause');
+const startButton = document.getElementById('start');
+
+const counter$ = interval(1000);
+const startClick$ = fromEvent(startButton, 'click');
+const pauseClick$ = fromEvent(pauseButton, 'click');
 
 merge(
-    keyup$,
-    click$
-).subscribe(console.log);
+    startClick$.pipe(mapTo(true)),
+    pauseClick$.pipe(mapTo(false))
+).pipe(
+    switchMap(shouldStart => {
+        return shouldStart ? counter$ : EMPTY
+    }),
+    mapTo(-1),
+    scan((accumulator, current) => {
+        return accumulator + current;
+    }, COUNTDOWN_FROM),
+    takeWhile(value => value >= 0),
+    startWith(COUNTDOWN_FROM)
+).subscribe(value => {
+    countdown.innerHTML = value;
+    if (!value) {
+        message.innerHTML = 'Liftoff!!';
+    }
+})
