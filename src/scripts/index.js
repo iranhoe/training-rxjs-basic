8fbf3ef43ef54779c6ce248a215c4dd21743f2be
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-12 17:03:01 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,12 @@
-import { fromEvent } from "rxjs";
+import { of, forkJoin } from "rxjs";
+import { delay } from "rxjs/operators";
+
+
+// streams
+const numbers$ = of(1,2,3);
+const letters$ = of('a', 'b', 'c');
+
+forkJoin(
+    numbers$,
+    letters$.pipe(delay(3000))
+).subscribe(console.log);
